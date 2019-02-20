ce9b9d656fd97ecd3ddd51ed81865a236c489904
Bart <salemon876@gmail.com>
2019-02-20 15:32:23 +0100

    update added

--- src/main/java/com/webndb/wb/CustomerRepository.java
+++ src/main/java/com/webndb/wb/CustomerRepository.java
@@ -2,11 +2,11 @@ package com.webndb.wb;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends CrudRepository<Patient, Long> {
 
     Iterable<Patient> findAll();
     Optional<Patient> findById(Long id);
+    void deleteById(Long id);
 }
