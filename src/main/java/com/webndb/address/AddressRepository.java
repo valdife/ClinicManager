b8dc1c64a0134d84caa4831d1d4ec00aed9e0de6
Bart <salemon876@gmail.com>
2019-02-20 16:51:14 +0100

    fix

---
+++ src/main/java/com/webndb/address/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.webndb.address;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AddressRepository extends CrudRepository<Address, Long> {
+
+
+    Iterable<Address> findAllByResort (String resort);
+}
--- src/main/java/com/webndb/wb/WbApplication.java
+++ src/main/java/com/webndb/wb/WbApplication.java
@@ -22,15 +22,6 @@ public class WbApplication {
     }
 }
 
-// save a couple of customers //String pesel, float height, int weight
-// fetch all customers
-//            log.info("Customers found with findAll():");
-//            log.info("-------------------------------");
-//            for (Patient patient : repository.findAll()) {
-//                log.info(patient.getFirstName());
-//            }
-//            log.info("-------------------------------");
-
 // fetch an individual customer by ID
 //            repository.findById(1L)
 //                    .ifPresent(customer -> {
@@ -39,14 +30,9 @@ public class WbApplication {
 //                        log.info(customer.toString());
 //                        log.info("");
 //                    });
+
 // fetch customers by last name
 //            log.info("Customer found with findByLastName('Bauer'):");
 //            log.info("--------------------------------------------");
 //            repository.findByLastName("Bauer").forEach(bauer -> log.info(bauer.toString()));
 //            repository.findByFirstName("David").forEach(david -> log.info(david.toString()));
-
-
-// for (Customer bauer : repository.findByLastName("Bauer")) {
-// 	log.info(bauer.toString());
-// }
-//log.info("TEST TEST TEST");
