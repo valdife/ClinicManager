97e5123a5f25ad0f5b5af76491325689fae371fd
valdife <salemon876@gmail.com>
2019-03-07 12:50:05 +0100

    Changed JpaRepo to CrudRepo

--- src/main/java/com/webndb/wb/PatientRepository.java
+++ src/main/java/com/webndb/wb/PatientRepository.java
@@ -1,11 +1,10 @@
 package com.webndb.wb;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface PatientRepository extends JpaRepository<Patient, Long> {
+public interface PatientRepository extends CrudRepository<Patient, Long> {
 
     Optional<Patient> findById(Long id);
     void deleteById(Long id);
