f60549b20729212e6f7d37c185b0a6854a886079
valdife <salemon876@gmail.com>
2019-03-07 12:55:45 +0100

    Fixed conflict in AddressRepo

--- src/main/java/com/webndb/address/AddressRepository.java
+++ src/main/java/com/webndb/address/AddressRepository.java
@@ -2,11 +2,6 @@ package com.webndb.address;
 
 import org.springframework.data.repository.CrudRepository;
 
-<<<<<<< HEAD
-=======
-import java.util.Set;
-
->>>>>>> develop/address_service
 public interface AddressRepository extends CrudRepository<Address, Long> {
 
     Iterable<Address> findAllByResort (String resort);
